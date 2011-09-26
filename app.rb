1103b0797573a2979ad5e5adc87e11da4fe3c621
Mark Guerra <markgx@gmail.com>
2011-09-26 00:09:44 -0700

    need this param

--- app.rb
+++ app.rb
@@ -22,7 +22,7 @@ get '/login' do
 end
 
 get '/callback' do
-  response = Instagram.get_access_token(params[:code])
+  response = Instagram.get_access_token(params[:code], :redirect_uri => CALLBACK_URL)
   session[:access_token] = response.access_token
   redirect '/feed'
 end
